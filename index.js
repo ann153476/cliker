b5dabfdc8ed3ccda7c5f7c70c025f9a5e751ec13
Anka <103377387+ann153476@users.noreply.github.com>
2023-05-01 22:41:21 +0300

    all

---
+++ index.js
@@ -0,0 +1,28 @@
+const click = document.querySelector(".click");
+const number = document.querySelector(".number");
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed");
+
+let value = 0;
+let rotate = 0;
+console.log(click);
+const clickF = (e) => {
+  if (rotate < 178) {
+    value += 1;
+    rotate += 1;
+    number.innerHTML = `${value}`;
+    arrow.style.transform = `rotate3d(0, 0, 1, ${rotate}deg)`;
+  }
+};
+click.addEventListener("click", clickF);
+var intervalID = window.setInterval(myCallback, 1000);
+let valueSecond = 0;
+function myCallback() {
+  if (rotate > 1) {
+    speed.innerHTML = `${value - valueSecond}`;
+    rotate -= 2;
+    arrow.style.transform = `rotate3d(0, 0, 1, ${rotate}deg)`;
+    console.log(rotate);
+  }
+  valueSecond = value;
+}
